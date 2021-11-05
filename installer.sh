ed25ebb3a39c4201fe0d49bffd37ae0cca0ad5b8
Alisa Nesterenko <kot8988@ya.ru>
2021-11-05 12:48:27 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,6 +2,7 @@ sudo apt-get update
 sudo apt-get install build-essential
 sudo apt-get install qtcreator
 sudo apt install qt5-default
+sudo apt-get install qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
 git clone https://github.com/cutehater/FunctionDrawer.git
 mkdir FunctionDrawer/build
 cd FunctionDrawer/build
